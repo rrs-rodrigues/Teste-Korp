1e5a51a23709d56dc66f1b6d49c492dafe0662c1
Rogerio Rodrigues da Silva <rrs_rodrigues@outlook.com>
2026-08-20 22:57:38 -0300

    Organiza arquivos de configuração do Ansible e adiciona script de inicialização do projeto

---
+++ instala ansible e inicia projeto.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#echo "Iniciando instalação do Ansible" 
+#
+#sudo apt-get update -y && apt-get install ansible -y 
+#
+#echo "Instalação do Ansible concluída"
+#
+#sleep 2
+#
+echo "Iniciando projeto"
+
+cd ansible
+
+ansible-playbook install-docker.yml
+
+cd ../
+
+echo "Projeto iniciado com sucesso"
+
